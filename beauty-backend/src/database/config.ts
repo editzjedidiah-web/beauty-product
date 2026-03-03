f3d11296d53706827c034fc67c175560f94ad0ba
Bendicion <editzjedidiah@gmail.com>
2026-03-03 14:50:14 +0300

    Update config.ts

--- beauty-backend/src/database/config.ts
+++ beauty-backend/src/database/config.ts
@@ -1,1 +1,10 @@
+import { Sequelize } from 'sequilize';
+import path from 'path';
+//This points to the /server/database.sqlite file 
+const sequelize = Sequelize({
+  dialect; 'sqlite',
+  storage: path.join(__dirname, '../../server/database.sqlite'),
+  logging: false, //Set to console.log if you want to see raw SQL queries
+}), 
 
+  export default sequelize; 
